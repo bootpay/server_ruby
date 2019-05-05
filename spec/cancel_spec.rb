645333fcae37d508ca9e82cdc347ca391fc3ed9e
Gosomi <gosomi@forelf.com>
2019-05-05 16:30:14 +0900

    네이버페이 취소 테스트 코드 추가

--- spec/cancel_spec.rb
+++ spec/cancel_spec.rb
@@ -2,9 +2,15 @@ require "spec_helper"
 
 RSpec.describe Bootpay do
   it 'Receipt Cancel' do
+    # bootpay = Bootpay::ServerApi.new(
+    #   '59bfc738e13f337dbd6ca48a',
+    #   'pDc0NwlkEX3aSaHTp/PPL/i8vn5E/CqRChgyEp/gHD0=',
+    #   :development
+    # )
+
     bootpay = Bootpay::ServerApi.new(
-      '59bfc738e13f337dbd6ca48a',
-      'pDc0NwlkEX3aSaHTp/PPL/i8vn5E/CqRChgyEp/gHD0=',
+      '5cb93559e13f331a427e8f26',
+      'RHa8MMvNJF1dNucxDWrVUmg5j4HMf/Kdz98fSytrnIk=',
       :development
     )
     result  = bootpay.get_access_token
@@ -12,11 +18,11 @@ RSpec.describe Bootpay do
     expect(result[:status]).to eq(200)
 
     result = bootpay.cancel(
-      '5cb7cbd0e13f335f42609278',
-      2000,
-      1500,
-      '테스트',
-      '테스트'
+      '5cce90afe13f331f6f5b590a',
+      '35000',
+      0,
+      'test',
+      'test'
     )
     print result
   end
