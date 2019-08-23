ec93463caea6b39a48bbf96e5deeb40a944315d1
ehowlsla <jungler@cvdv.me>
2019-08-23 13:01:46 +0900

    remote form added

--- lib/bootpay/server_api.rb
+++ lib/bootpay/server_api.rb
@@ -165,6 +165,24 @@ module Bootpay
       )
     end
 
+    def remote_form(remote_form, sms_payload = nil)
+      raise 'token 값이 비어 있습니다.' if @token.blank?
+      raise 'remote_form 값이 비어 있습니다.' if data[:remote_form].blank?
+      request(
+          :post,
+          [api_url, 'app', 'rest', 'remote_form.json'].join('/'),
+          {
+              application_id: @application_id,
+              remote: remote_form,
+              sms_payload: sms_payload
+          },
+          {
+              content_type:  :json,
+              Authorization: @token
+          }
+      )
+    end
+
     def send_sms(receive_numbers, message, send_number = nil, extra = {})
       request(
         :post,
