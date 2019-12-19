6993835f5013437b566ce71f9cd100b8d8c9095a
Gosomi <gosomi@forelf.com>
2019-12-19 10:46:49 +0900

    rspec 업데이트

--- spec/certificate_spec.rb
+++ spec/certificate_spec.rb
@@ -4,21 +4,13 @@ RSpec.describe Bootpay do
   it 'Certificate Verify' do
     bootpay = Bootpay::ServerApi.new(
       '59bfc738e13f337dbd6ca48a',
-      'pDc0NwlkEX3aSaHTp/PPL/i8vn5E/CqRChgyEp/gHD0=',
-      :development
+      'pDc0NwlkEX3aSaHTp/PPL/i8vn5E/CqRChgyEp/gHD0='
     )
-    # 59b731f084382614ebf72215
-    # WwDv0UjfwFa04wYG0LJZZv1xwraQnlhnHE375n52X0U=
-    #bootpay = Bootpay::ServerApi.new(
-    #  '59b731f084382614ebf72215',
-    #  'WwDv0UjfwFa04wYG0LJZZv1xwraQnlhnHE375n52X0U=',
-    #  :stage
-    #)
-    result = bootpay.get_access_token
+    result  = bootpay.get_access_token
     print result
     expect(result).not_to be_empty
     expect(result[:status]).to eq(200)
-    result = bootpay.certificate("5df6ec63867da503738991b7")
+    result = bootpay.certificate("[[ receipt_id ]]")
     print result.to_json
   end
 end
