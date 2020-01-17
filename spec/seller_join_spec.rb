5e6ab76f60d4228a45754c110516919dc524df4e
Gosomi <gosomi@forelf.com>
2020-01-17 14:02:46 +0900

    active support 버전 고정 가맹점 가입 로직 추가

--- spec/seller_join_spec.rb
+++ spec/seller_join_spec.rb
@@ -14,19 +14,18 @@ RSpec.describe Bootpay do
     #     'WwDv0UjfwFa04wYG0LJZZv1xwraQnlhnHE375n52X0U='
     # )
     result = bootpay.get_access_token
-    print result
     expect(result).not_to be_empty
     expect(result[:status]).to eq(200)
     result = bootpay.seller_join(
-      email:        'test_bootpay@bootpay.co.kr',
-      password:     '__RANDOM__',
-      phone:        '01000000000',
-      seller_name:  '부트페이',
-      company_name: '부트페이',
-      owner_name:   '부트페이 대표',
-      app_name:     '부트페페이',
-      default_app:  1,
-      destroy:      1
+      email:          'test_bootpay@bootpay.co.kr',
+      password:       '__RANDOM__',
+      phone:          '01000000000',
+      seller_name:    '부트페이',
+      company_name:   '부트페이',
+      owner_name:     '부트페이 대표',
+      app_name:       '부트페페이',
+      default_app:    1,
+      destroy:        1
     )
     print result.to_json
   end
